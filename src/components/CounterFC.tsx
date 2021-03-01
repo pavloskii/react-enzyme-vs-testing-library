11ea5cf0a80ecb95abcd081cce2d6fd734838fc8
pavloskii <pavlo.igor@gmail.com>
2021-03-01 01:32:06 +0100

    RTL setup

---
+++ src/components/CounterFC.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+interface CounterProps {
+  min?: number;
+  max?: number;
+}
+
+const CounterFC = ({ min = 0, max = 10 }: CounterProps) => {
+  const [counter, setCounter] = React.useState(min);
+  const [hasEdited, setHasEdited] = React.useState(false);
+
+  React.useEffect(() => {
+    if (counter !== min && !hasEdited) {
+      setHasEdited(true);
+    }
+  }, [counter, hasEdited, min]);
+
+  return (
+    <div className="RangeCounter">
+      <span className="RangeCounter__title">Counter (Function Comp)</span>
+      <div className="RangeCounter__controls">
+        <button
+          disabled={counter <= min}
+          onClick={() => setCounter(counter - 1)}
+        >
+          -
+        </button>
+        <span data-testid="counter-value">{counter}</span>
+        <button
+          disabled={counter >= max}
+          onClick={() => setCounter(counter + 1)}
+        >
+          +
+        </button>
+      </div>
+      {(counter >= max || counter <= min) && hasEdited && (
+        <span className="RangeCounter__alert">Range limit reached!</span>
+      )}
+    </div>
+  );
+};
+
+export default CounterFC;
